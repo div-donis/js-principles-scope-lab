1fd7fb0ddfdbd438e80de425ba7dd2473486877b
Donovan <divdonis@gmail.com>
2021-05-02 15:57:51 -0700

    commit tree 3689d8f4a7774c1fed41ba4bd9b0412b9969476a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+customerName = 'bob';
+
+/*function name(){
+    return customerName;
+}*/
+
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase(); 
+     return customerName; 
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'mark';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'maybe mark';
+    return leastFavoriteCustomer;
+}
